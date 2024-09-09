57c411eaa401ba43e9f0d2e953b1f47963736794
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-09-09 17:06:41 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const nome = "Murata"
+
+const meuObj = {
+  nome: "Guilherme Murata",
+  filmeFav: "Openheimer"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+</script>
+
+<template>
+  <h1>
+    {{ dizOla("Paula") }}
+  </h1>
+</template>
+
+<style scoped>
+</style>
